2f9492ec355cd648909214de2eedd0dd78ef8a6f
Ilia Krylatykh <iliakrylatykh@gmail.com>
2022-06-29 16:58:16 +0500

    new: film count

--- script.js
+++ script.js
@@ -8,12 +8,33 @@ const personalMuvieDB = {
   privat: false,
 };
 
-const a = prompt("Один из просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?"),
-  c = prompt("Один из просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?");
+for (let i = 0; i < numberOfFilms; i++) {
+  const a = prompt("Один из просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
 
-personalMuvieDB.muvies[a] = b;
-personalMuvieDB.muvies[c] = d;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMuvieDB.muvies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+
+  if (i === numberOfFilms) {
+    break;
+  }
+}
+
+if (personalMuvieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMuvieDB.count >= 10 && personalMuvieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMuvieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMuvieDB);
+
+console.log(personalMuvieDB.count);
